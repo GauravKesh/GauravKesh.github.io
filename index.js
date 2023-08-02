cb11bf4b01c7cce3f7dc1c01b7de4cc46d38c4f8
Gaurav Kesh Roushan <gkrcoder@gmail.com>
2023-08-03 02:48:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-// creating cookie
-
-function setcookie(cname,cvalue,exday){
-  let date = new Date();
-  date.setTime(date.getTime() + (exday*24*60*60*1000));
-  let expires = "expires="+date.toUTCString();
-  document.cookie=cname +"="+ cname+"="+";" + cvalue + ";" + expires +";path=/";
+function setCookie(cname,cvalue,exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
-function getcookie(cname){
-  let name = cname+"=";
+function getCookie(cname) {
+  let name = cname + "=";
   let decodedCookie = decodeURIComponent(document.cookie);
-  let ca  = decodedCookie.split(";");
-  let calen = ca.length;
-  for(let i = 0; i < calen; i++) {
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
     let c = ca[i];
     while (c.charAt(0) == ' ') {
       c = c.substring(1);
@@ -24,16 +21,14 @@ function getcookie(cname){
   return "";
 }
 
-function checkcookie(){
-  let user = getcookie("username");
-  if(user!=""){
-    alert("welxome" + user);
-  }else{
-    user =prompt("Enter your name");
-    if(user!="" && user!=null){
-      setcookie("username",user,0);
-    }
-    
+function checkCookie() {
+  let user = getCookie("username");
+  if (user != "") {
+    alert("Welcome again " + user);
+  } else {
+     user = prompt("Please enter your name:","");
+     if (user != "" && user != null) {
+       setCookie("username", user, 30);
+     }
   }
-  
 }
